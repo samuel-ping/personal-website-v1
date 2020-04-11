aaa317eb19b5af8c07080c3fd8fb75f50dc197f9
Sam Ping <samuelping42@gmail.com>
2020-04-11 19:56:53 -0400

    added resume button

--- src/components/LandingComponents/Resumebar.jsx
+++ src/components/LandingComponents/Resumebar.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from "react";
 
 import Resume from "../../assets/samuel-ping.3-26-20.pdf";
-import Github from "../../assets/images/icons/github-icon.png";
-import LinkedIn from "../../assets/images/icons/linkedin-icon.png";
-import Email from "../../assets/images/icons/email-icon.png";
+import ResumeIcon from "../../assets/images/icons/resume-icon.png";
+import GitHubIcon from "../../assets/images/icons/github-icon.png";
+import LinkedInIcon from "../../assets/images/icons/linkedin-icon.png";
+import EmailIcon from "../../assets/images/icons/email-icon.png";
 
 export default class Resumebar extends Component {
   render() {
     return (
       <ul id="icon-bar">
-        {/* <li class="resume-button">
-          <a href={Resume}></a>
-        </li> */}
+        <li class="icon-button">
+          <a href={Resume} target="_blank">
+            <img src={ResumeIcon} />
+          </a>
+        </li>
         <li class="icon-button">
           <a href="https://github.com/samuel-ping" target="_blank">
-            <img src={Github} />
+            <img src={GitHubIcon} />
           </a>
         </li>
         <li class="icon-button">
           <a href="https://linkedin.com/in/samuelping" target="_blank">
-            <img src={LinkedIn} />
+            <img src={LinkedInIcon} />
           </a>
         </li>
         <li class="icon-button">
@@ -27,7 +30,7 @@ export default class Resumebar extends Component {
             href="mailto:samuel.y.ping@gmail.com?Subject=Hey%20there!"
             target="_top"
           >
-            <img src={Email} />
+            <img src={EmailIcon} />
           </a>
         </li>
       </ul>
--- src/components/LandingComponents/ResumebarComponents/ResumeButton.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class ResumeButton extends Component {
-  render() {
-    return null;
-  }
-}
