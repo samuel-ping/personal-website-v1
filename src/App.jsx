2e955a8d82bf1f3fe6de378b90f1561b60f48d7d
Sam Ping <samuelping42@gmail.com>
2020-04-12 23:57:54 -0400

    updating GA to track all pages and fixing favicon on Chrome

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
 import ReactGA from "react-ga";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import Landing from "./components/Landing";
 import Navbar from "./components/Navbar";
@@ -8,16 +10,25 @@ import Credits from "./components/Credits";
 
 import "./components/styles/General.css";
 
+const history = createBrowserHistory();
+
 ReactGA.initialize("UA-163484661-1");
-ReactGA.pageview("/about-me");
+// ReactGA.pageview("/about-me");
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Landing />
-        <Navbar />
-        <Credits />
+        <Router history={history}>
+          <Landing />
+          <Navbar />
+          <Credits />
+        </Router>
       </div>
     );
   }
