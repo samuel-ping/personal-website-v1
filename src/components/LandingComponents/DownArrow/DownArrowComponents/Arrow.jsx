d9f78481ff729a5bebec787a595959f263ad10d6
Sam Ping <samuelping42@gmail.com>
2020-05-23 13:25:04 -0400

    Updates

--- src/components/LandingComponents/DownArrow/DownArrowComponents/Arrow.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Arrow extends Component {
-  render() {
-    return <div id="down-arrow" />;
-  }
-}
-
-export default Arrow;
--- src/components/NavbarTabs/NavbarTabComponents/CardComponents/ImagesBank.js
+++
@@ -1,6 +0,0 @@
-const picturePreviews = {
-  portraitOfMe: require("../../../../assets/images/portrait-full.jpg"),
-  personalWebsitePreview: require("../../../../assets/images/personal-website-preview.png"),
-  foodMoodPreview: require("../../../../assets/images/food-mood-website-preview.png"),
-};
-module.exports = picturePreviews;
